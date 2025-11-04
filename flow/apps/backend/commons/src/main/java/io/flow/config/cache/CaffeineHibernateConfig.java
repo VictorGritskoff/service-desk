1434d9b506bb4488e6a5b585d428776d19abd4f6
Victor Gritskoff <143332387+VictorGritskoff@users.noreply.github.com>
2025-11-04 22:59:50 +0300

    Add files via upload

---
+++ flow/apps/backend/commons/src/main/java/io/flow/config/cache/CaffeineHibernateConfig.java
@@ -0,0 +1,14 @@
+package io.flow.config.cache;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConditionalOnProperty(
+        prefix = "flow.hibernate.second_level_cache",
+        name = "provider",
+        havingValue = "caffeine",
+        matchIfMissing = true)
+@PropertySource("classpath:cache/hibernate-caffeine.properties")
+public class CaffeineHibernateConfig {}
