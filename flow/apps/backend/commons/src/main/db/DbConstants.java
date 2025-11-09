761d1847c66fe2c7047e1142d6254e5974aad2f9
Victor Gritskoff <143332387+VictorGritskoff@users.noreply.github.com>
2025-11-09 14:57:46 +0300

    Database configuration

---
+++ flow/apps/backend/commons/src/main/db/DbConstants.java
@@ -0,0 +1,8 @@
+package io.flow.db;
+
+public interface DbConstants {
+
+    String DEFAULT_TENANT = "flow";
+
+    String TENANT_CHANGESET = "config/liquibase/tenant/master.xml";
+}
---
+++ flow/apps/backend/commons/src/main/db/service/LiquibaseService.java
@@ -0,0 +1,48 @@
+package io.flow.db.service;
+
+import java.sql.Connection;
+import java.util.Collection;
+import javax.sql.DataSource;
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class LiquibaseService {
+
+    private final DataSource dataSource;
+
+    public LiquibaseService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Transactional
+    @SneakyThrows
+    public void updateLiquibaseSchema(
+            String classpathChangeset, String schema, Collection<String> activeProfiles) {
+        try (Connection connection = dataSource.getConnection()) {
+            log.info("Going to create a schema {}", schema);
+            connection.prepareCall("CREATE SCHEMA IF NOT EXISTS " + schema).execute();
+            // Create the database for the default tenant
+            Database database =
+                    DatabaseFactory.getInstance()
+                            .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            database.setDefaultSchemaName(schema);
+            Liquibase liquibase =
+                    new Liquibase(classpathChangeset, new ClassLoaderResourceAccessor(), database);
+            Contexts contexts = new Contexts();
+            activeProfiles.forEach(contexts::add);
+            liquibase.update(contexts, new LabelExpression());
+            liquibase.close();
+        }
+    }
+}
